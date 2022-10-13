cef2a2e40c75eba412dbd6039452a3eb1eb2d7a4
Pye <pye@picnichealth.com>
2022-10-13 12:49:02 -0700

    Load blanket

--- setup.sh
+++ setup.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
-sudo apt install silversearcher-ag
-
+# setup lsp dependencies
 npm i -g typescript typescript-language-server
+
+# setup emacs
+sudo add-apt-repository -y ppa:kelleyk/emacs
+sudo apt update
+
+sudo apt remove emacs*
+sudo apt install -y silversearcher-ag emacs28
+git clone https://github.com/pyephyomaung/.emacs.d.git /workspace/.emacs.d
+
+echo '(load-file "/workspace/.emacs.d/init.el")' >> ~/.emacs
+mkdir -p ~/.local/share/fonts
+cp /workspace/.emacs.d/*.ttf ~/.local/share/fonts
+fc-cache -f -v
+
+emacs --daemon
+alias ec="emacs-client -c"
+
+# setup blanket
+git clone https://github.picnichealth.com/picnichealth/utilities.git /workspace/utilities
+echo '(load-file "/workspace/utilities/blanket/blanket.el")' >> ~/.emacs
