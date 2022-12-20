248b594155c901be015326e817a161459adacbb9
Pye <pye@picnichealth.com>
2022-12-19 19:00:32 -0800

    Symlink first

--- setup.sh
+++ setup.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# install lsp dependencies
-npm i -g typescript typescript-language-server
-
-# install system dependencies for emacs
-brew install emacs the_silver_searcher
-
 # install emacs config
 git clone https://github.com/pyephyomaung/.emacs.d.git /workspace/.emacs.d
 mkdir -p ~/.local/share/fonts
@@ -16,3 +10,9 @@ ln -s /home/gitpod/.dotfiles/.emacs /home/gitpod/.emacs
 
 mkdir -p /workspace/.emacs.d
 ln -s /workspace/.emacs.d /home/gitpod/.emacs.d
+
+# install lsp dependencies
+npm i -g typescript typescript-language-server
+
+# install system dependencies for emacs
+brew install emacs the_silver_searcher
